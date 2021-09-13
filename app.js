c94a7ba618fa8acdc0bba218c4240c5f1060f1e2
adifdwimaulana <adif.maulana@thebonza.com>
2021-09-13 19:05:43 +0700

    feat: add cors restriction

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -12,6 +13,13 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://147.139.206.62'],
+    credentials: true, // access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
